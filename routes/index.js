bcbbc46e0b97ee020135753e3fb2aaaf57dbb821
SamillWong <samillwong@hotmail.com>
2021-05-09 04:20:50 +0930

    Add register page

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,25 @@ router.get('/login', function(req, res) {
   return res.sendFile('login.html', { root: 'views' });
 });
 
+/* POST login page */
+router.post('/login', function(req, res) {
+  console.log(req.body);
+  // TODO: Add input validation and implement server-side
+  return res.send("Success");
+});
+
+/* GET register page */
+router.get('/register', function(req, res) {
+  return res.sendFile('register.html', { root: 'views' });
+});
+
+/* POST register page */
+router.post('/register', function(req, res) {
+  console.log(req.body);
+  // TODO: Add input validation and implement server-side
+  return res.send("Success");
+});
+
 /* GET venue page */
 router.get('/venue', function(req, res) {
   return res.sendFile('venue.html', { root: 'views' });
