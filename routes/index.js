b692be2455bb5fc8a0f044930fefd0a791de0857
SamillWong <samillwong@hotmail.com>
2021-05-08 21:22:29 +0930

    Add login page and FontAwesome

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET home page */
+router.get('/', function(req, res) {
+  return res.sendFile('index.html', { root: 'views' });
+});
+
+/* GET login page */
+router.get('/login', function(req, res) {
+  return res.sendFile('login.html', { root: 'views' });
+});
+
+/* GET venue page */
+router.get('/venue', function(req, res) {
+  return res.sendFile('venue.html', { root: 'views' });
 });
 
 module.exports = router;
