3c9cbbbc97022807c1713d647401e1b85e45e9d6
SamillWong <samillwong@hotmail.com>
2021-06-10 22:33:30 +0930

    Fix session debug endpoint

--- routes/debug.js
+++ routes/debug.js
@@ -3,7 +3,7 @@ var router  = express.Router();
 
 /* DEBUG: Show session data */
 router.get('/session', function(req, res) {
-    return res.send(req.session);
+    return res.send(JSON.stringify(req.session));
 });
 
 module.exports = router;
