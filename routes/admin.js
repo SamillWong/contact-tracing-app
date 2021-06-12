6503ec658d86bbe8395560dea4151449969b649b
AdamMilne1 <adammilne222@gmail.com>
2021-06-12 13:18:20 +0000

    Added Health Official Search Page And Search Functions

--- routes/admin.js
+++ routes/admin.js
@@ -30,7 +30,6 @@ router.post('/register', function (req, res) {
         password: req.body.password,
         type: req.body.type
     };
-
     newUser.type='healthofficial'
 
     // Overwrite sent password with new hashed/salted password
@@ -38,12 +37,10 @@ router.post('/register', function (req, res) {
     const hash = bcrypt.hashSync(newUser.password, salt);
     newUser.password = hash;
 
-
     req.pool.getConnection(function(err, connection){
 
         var selectQuery = "SELECT Email FROM User WHERE Email=? UNION SELECT Email FROM VenueManager WHERE Email = ? UNION SELECT Email FROM HealthOfficial WHERE Email = ? ;";
         var insertQuery = "INSERT INTO HealthOfficial (Email, Password, FirstName, LastName) VALUES (?, ?, ?, ?);";
-
 
         getPromise = (query, param) => {
             return new Promise((resolve, reject) => {
@@ -53,7 +50,6 @@ router.post('/register', function (req, res) {
                 })
             });
         }
-
         async function register() {
             try {
                 var promises = [
@@ -65,7 +61,6 @@ router.post('/register', function (req, res) {
                 console.log(err);
             }
         }
-
         // Checks if email already exists
         connection.query(selectQuery, [newUser.email, newUser.email, newUser.email], async function (err, rows, fields) {
             if (err) {
@@ -86,13 +81,7 @@ router.post('/register', function (req, res) {
                 res.redirect('/login');
             }
         });
-
-
-
-
     });
-
-
 });
 
 /*
@@ -126,12 +115,41 @@ router.post('/hotspots', function (req, res) {
  * Admins should be able to manage users.
  */
 router.get('/users', function (req, res) {
-    return res.sendFile('admin-users.html', { root: 'views' });
+    return res.render('search.ejs', { params: { verified: req.session.verified } });
 });
 
 router.post('/users', function (req, res) {
-    // TODO: Implement server-side
-    return res.send("Success");
+
+    req.pool.getConnection(async function (err, connection) {
+        if (err) {
+            res.sendStatus(500);
+        }
+
+        // Query the database with account ID and return a Promise object
+        getPromise = (query, param) => {
+            return new Promise((resolve, reject) => {
+                connection.query(query, param, function (err, rows, fields) {
+                    if (err) return reject(err);
+                    return resolve(rows);
+                });
+            });
+        }
+
+        // Construct rows from each query
+        async function makeQuery() {
+            var profileQuery = "SELECT UserID, Email, FIrstName, LastName, Address, ContactNumber FROM User WHERE Email = ?;";
+            try {
+                const promises = [getPromise(profileQuery, [req.body.email])];
+                return await Promise.all(promises);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        var result = await makeQuery();
+        connection.release();
+
+        res.send(JSON.stringify(result[0][0]));
+    });
 });
 
 /*
@@ -139,12 +157,42 @@ router.post('/users', function (req, res) {
  * Admins should be able to manage venues.
  */
 router.get('/venues', function (req, res) {
-    return res.sendFile('admin-venues.html', { root: 'views' });
+    return res.render('search.ejs', { params: { verified: req.session.verified } });
 });
 
 router.post('/venues', function (req, res) {
-    // TODO: Implement server-side
-    return res.send("Success");
+
+    req.pool.getConnection(async function (err, connection) {
+        if (err) {
+            res.sendStatus(500);
+        }
+
+        // Query the database with Email and return a Promise object
+        getPromise = (query, param) => {
+            return new Promise((resolve, reject) => {
+                connection.query(query, param, function (err, rows, fields) {
+                    if (err) return reject(err);
+                    return resolve(rows);
+                });
+            });
+        }
+
+        // Construct rows from each query
+        async function makeQuery() {
+            var profileQuery = "SELECT VenueID, Name, Venue.Address, Longitude, Latitude, Venue.ContactNumber, ManagerID, Email, FirstName, LastName FROM Venue INNER JOIN VenueManager ON Venue.VenueID = VenueManager.ManagerID WHERE Email = ?;";
+            try {
+                const promises = [getPromise(profileQuery, [req.body.email])];
+                return await Promise.all(promises);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        var result = await makeQuery();
+        connection.release();
+
+        res.send(JSON.stringify(result[0][0]));
+    });
+
 });
 
 module.exports = router;
