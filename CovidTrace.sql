68846afd3bafeb655165c1fa3b1b0f32cc035fb7
SamillWong <samillwong@hotmail.com>
2021-06-12 16:44:18 +0930

    Remove redundant key in VenueManager table

--- CovidTrace.sql
+++ CovidTrace.sql
@@ -135,10 +135,7 @@ CREATE TABLE `VenueManager` (
   `LastName` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `Address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `ContactNumber` varchar(16) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `VenueID` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ManagerID`),
-  KEY `VenueID` (`VenueID`),
-  CONSTRAINT `VenueManager_ibfk_1` FOREIGN KEY (`VenueID`) REFERENCES `Venue` (`VenueID`) ON DELETE SET NULL
+  PRIMARY KEY (`ManagerID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
