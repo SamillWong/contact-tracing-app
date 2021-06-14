0a6a3318b4de162d75991e067d28dffec50f55ad
SamillWong <samillwong@hotmail.com>
2021-06-14 19:55:52 +0930

    Add health official management API routes

--- routes/api.js
+++ routes/api.js
@@ -60,6 +60,49 @@ router.get('/profile', function (req, res, next) {
     });
 });
 
+/* Endpoint for getting profile details of another user */
+router.get('/profile/:type/:id', function (req, res, next) {
+
+    if (req.session.verified != 3) {
+        return res.sendStatus(401);
+    }
+
+    req.pool.getConnection(async function (err, connection) {
+        if (err) {
+            res.sendStatus(500);
+        }
+
+        // Query the database with account ID and return a Promise object
+        getPromise = (query, param) => {
+            return new Promise((resolve, reject) => {
+                connection.query(query, param, function (err, rows, fields) {
+                    if (err) return reject(err);
+                    return resolve(rows);
+                });
+            });
+        }
+
+        // Construct rows from each query
+        async function makeQuery() {
+            switch (parseInt(req.params.type)) {
+                case 1: var profileQuery = "SELECT UserID, FirstName, LastName, Email, Address, ContactNumber FROM User WHERE UserID = ?;"; break;
+                case 2: var profileQuery = "SELECT ManagerID, FirstName, LastName, Email, Address, ContactNumber FROM VenueManager WHERE ManagerID = ?;"; break;
+                case 3: var profileQuery = "SELECT HealthOfficialID, FirstName, LastName, Email, Address, ContactNumber FROM HealthOfficial WHERE HealthOfficialID = ?;"; break;
+            }
+            try {
+                const promises = [getPromise(profileQuery, [req.params.id])];
+                return await Promise.all(promises);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        var result = await makeQuery();
+        connection.release();
+
+        res.send(JSON.stringify(result[0][0]));
+    });
+});
+
 /* Endpoint for getting check-in entries */
 router.get('/check-in', function (req, res, next) {
 
@@ -107,6 +150,51 @@ router.get('/check-in', function (req, res, next) {
         res.send(JSON.stringify(result[0]));
     });
 });
+
+/* Endpoint for getting check-in entries for a user or venue */
+router.get('/check-in/:type/:id', function (req, res, next) {
+
+    if (req.session.verified != 3) {
+        return res.sendStatus(401);
+    }
+
+    req.pool.getConnection(async function (err, connection) {
+        if (err) {
+            res.sendStatus(500);
+        }
+
+        // Query the database with account ID and return a Promise object
+        getPromise = (query, param) => {
+            return new Promise((resolve, reject) => {
+                connection.query(query, param, function (err, rows, fields) {
+                    if (err) return reject(err);
+                    return resolve(rows);
+                });
+            });
+        }
+
+        // Construct rows from each query
+        async function makeQuery() {
+            switch (parseInt(req.params.type)) {
+                case 1:
+                    var checkInQuery = "SELECT CheckIn.CheckInID, CheckIn.Date, Venue.Name, Venue.Address FROM CheckIn INNER JOIN Venue ON Venue.VenueID=CheckIn.VenueID WHERE CheckIn.UserID = ? ORDER BY CheckIn.CheckInID;"; break;
+                case 2:
+                    var checkInQuery = "SELECT CheckIn.CheckInID, CheckIn.Date, User.FirstName, User.LastName FROM CheckIn INNER JOIN User ON CheckIn.UserID = User.UserID WHERE CheckIn.VenueID = ? ORDER BY CheckIn.CheckInID;"; break;
+            }
+            try {
+                const promises = [getPromise(checkInQuery, req.params.id)];
+                return await Promise.all(promises);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        var result = await makeQuery();
+        connection.release();
+
+        res.send(JSON.stringify(result[0]));
+    });
+});
+
 
 /* Endpoint for getting hotspot entries */
 router.get('/hotspot', function (req, res, next) {
@@ -209,6 +297,45 @@ router.get('/venue', function (req, res, next) {
             const venueQuery = "SELECT Venue.Name, Venue.Address, VenueManager.FirstName, VenueManager.LastName FROM Venue INNER JOIN VenueManager ON Venue.VenueID = VenueManager.ManagerID WHERE ManagerID = ?;";
             try {
                 const promises = [getPromise(venueQuery)];
+                return await Promise.all(promises);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        var result = await makeQuery();
+        connection.release();
+
+        res.send(JSON.stringify(result[0][0]));
+    });
+});
+
+/* Endpoint for getting profile details of another user */
+router.get('/venue/:id', function (req, res, next) {
+
+    if (req.session.verified != 3) {
+        return res.sendStatus(401);
+    }
+
+    req.pool.getConnection(async function (err, connection) {
+        if (err) {
+            res.sendStatus(500);
+        }
+
+        // Query the database with account ID and return a Promise object
+        getPromise = (query, param) => {
+            return new Promise((resolve, reject) => {
+                connection.query(query, param, function (err, rows, fields) {
+                    if (err) return reject(err);
+                    return resolve(rows);
+                });
+            });
+        }
+
+        // Construct rows from each query
+        async function makeQuery() {
+            var venueQuery = "SELECT Venue.VenueID, Venue.Name, Venue.Address, VenueManager.FirstName, VenueManager.LastName FROM Venue INNER JOIN VenueManager ON Venue.VenueID = VenueManager.ManagerID WHERE VenueID = ?;";
+            try {
+                const promises = [getPromise(venueQuery, [req.params.id])];
                 return await Promise.all(promises);
             } catch (err) {
                 console.log(err);
